3c051478c9810fce350b82009818f3b6f36e91c2
Gireesh Kumar <gireesh@github.com>
2017-12-01 18:06:36 +0530

    Commited on 1-12-2017

---
+++ MaxShopping/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingSite.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage="Please enter UserName",AllowEmptyStrings=false)]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage="Please enter Password",AllowEmptyStrings=false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
