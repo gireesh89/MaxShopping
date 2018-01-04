51e091e36095a2be4c4e675429946ace40ef61b3
Gireesh Kumar <gireesh@github.com>
2018-01-04 15:11:25 +0530

    04-01-2018
    Modified on 4-01-2018

---
+++ MaxShopping/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingSite.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        //
+        // GET: /Categories/
+        public ActionResult Categories()
+        {
+            return View();
+        }
+	}
+}
